abc358feab76fcd3882982fa278c8c91a0635c80
TuGitee <tgb@std.uestc.edu.cn>
2025-02-02 19:45:50 +0800

    feat: run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
+echo "Starting server..."
+nohup python app.py > flask.log 2>&1 &
+APP_PID=$!
+
 cd ./web/dist
-python -m http.server 3157
+echo "Starting web server..."
+nohup python -m http.server 5173 > web.log 2>&1 &
+
+wait $APP_PID
